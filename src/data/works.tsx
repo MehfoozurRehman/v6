40cc83438387cc56c4141a37120ac0689bf70fe6
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-04-22 09:07:54 +0500

    Update works.tsx

--- src/data/works.tsx
+++ src/data/works.tsx
@@ -1,37 +1,13 @@
 import { dsmeglobal } from "../assets";
 
 export const works = [
-  // {
-  //   company: "AIDA Pro",
-  //   name: "AIDA Pro",
-  //   tech: ["ReactJS", "ReactNative", "Sass", "Firebase", "Adobe Xd", "Rest"],
-  //   banner: aida,
-  //   info: "A website for startup to help companies and freelancers or jobseekers connect with candidates pre curated for companies according to customized requirements of companies",
-  //   siteLink: "https://aida-pro.web.app/",
-  // },
   {
     right: true,
     company: "DSME Global",
     name: "DSMEGlobals.com",
-    tech: ["ReactJS", "Sass", "Firebase", "Adobe Xd", "Rest"],
+    tech: ["React", "Sass", "Firebase", "Adobe XD", "REST"],
     banner: dsmeglobal,
-    info: "A website for a startup empowering and helping non technical personal to build creative and intuitive digital identity",
+    info: "DSME Global's website empowers non-technical individuals to create their digital identity with creativity and intuition.",
     siteLink: "http://www.dsmeglobal.co/",
   },
-  // {
-  //   name: "Barry ERP",
-  //   company: "Advanced AFF",
-  //   tech: [
-  //     "ReactJS",
-  //     "Sass",
-  //     "Express",
-  //     "NodeJS",
-  //     "MongoDB",
-  //     "Adobe Xd",
-  //     "Rest",
-  //   ],
-  //   banner: barry,
-  //   info: "A dashboard for a food flavor manufacturer to manage their inventory and sales and purchase orders and manage their employees and their salaries and attendance and much more",
-  //   siteLink: "https://erp.advancedff.com/",
-  // },
 ];
