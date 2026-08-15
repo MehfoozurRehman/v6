34a29dde58a82466e4bd111fe05dadb17f2c7726
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2026-08-15 08:14:42 +0500

    chore: simplify prettier config to 3 core options (semi, printWidth, singleQuote)

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,13 +1,7 @@
 const config = {
   semi: true,
-  endOfLine: 'lf',
   printWidth: 200,
   singleQuote: true,
-  trailingComma: 'es5',
-  tabWidth: 2,
-  useTabs: false,
-  bracketSpacing: true,
-  arrowParens: 'always',
 };
 
 export default config;
