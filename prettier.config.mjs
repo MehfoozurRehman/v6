8c75c367880c5bbfab58c2599677c35e4801a106
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2026-08-15 08:12:10 +0500

    chore: standardize prettier configuration to match schooliee

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+const config = {
+  semi: true,
+  endOfLine: 'lf',
+  printWidth: 200,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
+
+export default config;
